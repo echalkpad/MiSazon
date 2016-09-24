a237fa442c27967ac73ae611913177ae365f9589
Jhon Tello <jhontellolumbreras@gmail.com>
2016-09-24 17:57:55 -0500

    Cambie app.core

---
+++ app/pages/mireceta/mireceta.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the MirecetaPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/mireceta/mireceta.html',
+})
+export class MirecetaPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
